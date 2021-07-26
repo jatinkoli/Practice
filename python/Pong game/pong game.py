505d7755f3e9e400cd8ef2c51e7b5ee6b8859ab1
Jatin koli <kolijatin389@gmail.com>
2021-07-26 15:59:41 +0530

    Create pong game.py

---
+++ python/Pong game/pong game.py
@@ -0,0 +1,124 @@
+import turtle as t
+playerAscore=0
+playerAscore=0
+
+window=t.screen()
+window.title("Pong Game")
+window.bgcolor("red")
+window.setup(width=800,height=600)
+window.tracer(0)
+
+#creating left paddle
+leftpaddle=t.Turtle()
+leftpaddle.speed(0)
+leftpaddle.shape("square")
+leftpaddle.color("white")
+leftpaddle.shapesize(stretch_wid=5,stretch_len=1)
+leftpaddle.penup()
+leftpaddle.goto(-350,0)
+
+#creating right paddle
+rightpaddle=t.Turtle()
+rightpaddle.speed(0)
+rightpaddle.shape("square")
+rightpaddle.color("white")
+rightpaddle.shapesize(stretch_wid=5,stretch_len=1)
+rightpaddle.penup()
+rightpaddle.goto(350,0)
+
+
+#creating ball
+ball=t.Turtle()
+ball.speed(0)
+ball.shape("circlr")
+ball.color("green")
+ball.penup()
+ball.goto(5,5)
+ballxdirection=0.2
+ballydirection=0.2
+
+#creating pen for scorecard update
+pen=t.Turtle()
+pen.speed(0)
+pen.color("Blue")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("score",align="center",font=('Arial',24,'normal'))
+
+
+
+
+#moving the left paddle
+
+def leftpaddleup():
+    y=leftpaddle.ycor()
+    y=y+90
+    leftpaddle.sety(y)
+
+
+def leftpaddledown():
+    y=leftpaddle.ycor()
+    y=y-90
+    leftpaddle.sety(y)
+
+
+
+def rightpaddledown():
+    y=rightpaddle.ycor()
+    y=y+90
+    rightpaddle.sety(y)
+
+
+def rightpaddleup():
+    y=rightpaddle.ycor()
+    y=y-+90
+    rightpaddle.sety(y)
+
+#Assing keys to play
+
+window.listen()
+window.onkeypress(leftpaddleup,'w')
+window.onkeypress(leftpaddledown,'s')
+window.onkeypress(rightpaddleup,'up')
+window.onkeypress(rightpaddledown,'Down')
+
+while True:
+    window.update()
+
+    #moving the ball
+    ball.setx(ball.xcor()+ballxdirection)
+    ball.setx(ball.ycor()+ballxdirection)
+
+    #settingup border
+    if ball.ycor()>290:
+        ball.set(290)
+        ballydirection=ballydirection*-1
+    if ball.ycor()>-290:
+        ball.sety(-290)
+        ballydirection=ballydirection*-1
+
+    if ball.xcor()>390:
+        ball.goto(0,0)
+        ballxdirection=ballxdirection
+        playerAscore=playerAscore+1
+        pen.clear()
+        pen.write("player A:{}  player B:{}".format(playerAscore,playerBscore),align='center',font=('Arial',24,'normal'))
+
+    if (ball.xcor())<-390:
+        ball.goto(0,0)
+        ballxdirection=ballxdirection
+        playerBscore=playerBscore+1
+        pen.clear()
+        pen.write("player A:{}  player B:{}".format(playerAscore,playerBscore),align='center',font=('Arial',24,'normal'))
+
+
+     #Handling the collisions
+
+    if (ball.xcor()>340)and(ball.xcor()<350)and(ball.ycor()<rightpaddle.ycor()+40 and ball.ycor()>rightpaddle.ycor()-40)
+        ball.setx(340)
+        ballxdirection=ballxdirection*-1
+
+    if (ball.xcor()>-340)and(ball.xcor()<350)and(ball.ycor()<leftpaddle.ycor()+40 and ball.ycor()>leftpaddle.ycor()-40)
+        ball.setx(-340)
+        ballxdirection=ballxdirection*-1
