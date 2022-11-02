f3120e82505cc539ca5f4a1e98d5df12819a6e15
Jatin koli <kolijatin389@gmail.com>
2022-11-02 13:20:16 +0530

    Create Write a program to Division of two integer number.py

---
+++ python/Write a program to Division of two integer number/Write a program to Division of two integer number.py
@@ -0,0 +1,8 @@
+Write a program to Division of two integer number.py            # Write a program to Division of two integer number.
+
+A=int(input("Enter the first value: "))
+B=int(input("Enter the second value: "))
+
+C=A/B
+
+print("The division of {}/{}={}".format(A,B,C))
