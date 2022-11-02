6322b2fce38a18cf5bced572e05c1e4114883cd3
Jatin koli <kolijatin389@gmail.com>
2022-11-02 13:20:23 +0530

    Create Write a program to Multiplication of two integer number.py

---
+++ python/Write a program to Multiplication of two integer number/Write a program to Multiplication of two integer number.py
@@ -0,0 +1,8 @@
+        # Write a program to Multiplication of two integer number.
+
+A=int(input("Enter the first value: "))
+B=int(input("Enter the second value: "))
+
+C=A*B
+
+print("The multiplication of {}*{}={}".format(A,B,C))
