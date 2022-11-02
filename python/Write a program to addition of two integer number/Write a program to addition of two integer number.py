f4cfe08b5d3f69443d0eeed4d7497cbec9a77685
Jatin koli <kolijatin389@gmail.com>
2022-11-02 13:20:11 +0530

    Create Write a program to addition of two integer number.py

---
+++ python/Write a program to addition of two integer number/Write a program to addition of two integer number.py
@@ -0,0 +1,8 @@
+            # Write a program to addition of two integer number.
+
+A=int(input("Enter the first value: "))
+B=int(input("Enter the second value: "))
+
+C=A+B
+
+print("The addition of {}+{}={}".format(A,B,C))
