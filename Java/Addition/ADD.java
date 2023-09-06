3a0fd02333e4ef8d9d7ff0851886a1a6ae14148c
Jatin koli <kolijatin389@gmail.com>
2023-09-06 23:27:39 +0530

    Create ADD.java

---
+++ Java/Addition/ADD.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class ADD
+{
+
+    public static void main (String ar[])
+    {
+        int a,b;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter any two int numbers: ");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println("ADD of "+a+" + "+b+" = "+(a+b));
+    }
+}
