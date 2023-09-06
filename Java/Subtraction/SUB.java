96739507c08dd56946233c9afd6f1b0bb47e1b65
Jatin koli <kolijatin389@gmail.com>
2023-09-06 23:37:25 +0530

    .

---
+++ Java/Subtraction/SUB.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class SUB
+{
+    public static void main(String ar[])
+    {
+        int a,b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any 2 ing number: ");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println("SUB of "+a+" - "+b+" = "+(a-b));
+    }
+}
