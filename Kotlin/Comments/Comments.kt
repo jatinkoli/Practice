8a65ec81bff36bbbc0a0107afb68eb1556fc433a
Jatin koli <kolijatin389@gmail.com>
2024-02-09 23:27:26 +0530

    Create Comments.kt

---
+++ Kotlin/Comments/Comments.kt
@@ -0,0 +1,16 @@
+// Single-line Comments
+
+fun main ()
+{
+	println("Hello world") // this will print hello world 
+}
+
+
+/*Multi-line Comments*/
+
+fun main ()
+{
+	println("Hello world") 
+	/*println(3+3)
+	println("Jatin Koli")*/
+}
