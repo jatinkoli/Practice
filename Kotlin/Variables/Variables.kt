b57b016fc41144b11d4383b9b90ca46f9b39ab11
Jatin koli <kolijatin389@gmail.com>
2024-02-09 23:34:12 +0530

    Create Variables.kt

---
+++ Kotlin/Variables/Variables.kt
@@ -0,0 +1,17 @@
+/* Kotlin Variables
+
+To create a variable, use var or val, and assign a value to it with the equal sign (=):*/
+
+/*Syntax
+
+var variableName = value
+val variableName = value */
+
+fun main()
+{
+var name = "Jatin Koli"
+val birthyear = 2003
+
+println(name)          // Print the value of name
+println(birthyear)     // Print the value of birthyear
+}
