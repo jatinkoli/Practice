d94dbe2de46fb2190abb3118be6d4f58e3019c00
Jatin koli <kolijatin389@gmail.com>
2024-02-10 23:00:23 +0530

    Create Float.kt

---
+++ Kotlin/Data Types/1)Numbers/Float.kt
@@ -0,0 +1,17 @@
+/*Floating Point Types
+Floating point types represent numbers with a decimal, such as 9.99 or 3.14515.
+
+The Float and Double data types can store fractional numbers:*/
+
+
+fun main(args: Array<String>)
+{
+	// Float Example
+  val myNum: Float = 5.75F
+  	println(myNum)
+  
+	//Double Example
+  val myNumber: Double = 19.99
+	println(myNumber)
+}
+
