8fb6eb87f4755c4f8826b93e279b97ded261cae9
Jatin koli <kolijatin389@gmail.com>
2024-02-10 20:38:00 +0530

    Create Data_Types.kt

---
+++ Kotlin/Data Types/Data_Types.kt
@@ -0,0 +1,10 @@
+// Data Types 
+
+fun main()
+{
+	val Age_in_Number: Int = 5                // Int
+	val HeightDouble: Double = 5.99    // Double
+	val Name: Char = 'D'          // Char
+	val BooleanValues: Boolean = true     // Boolean
+	val Location: String = "Mumbai"      // String
+}
