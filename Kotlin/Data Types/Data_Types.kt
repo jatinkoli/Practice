29603cac83ac51bca1e995a6d4bb0e2628e864cd
Jatin koli <kolijatin389@gmail.com>
2024-02-10 20:40:05 +0530

    Update Data_Types.kt

--- Kotlin/Data Types/Data_Types.kt
+++ Kotlin/Data Types/Data_Types.kt
@@ -4,7 +4,7 @@ fun main()
 {
 	val Age_in_Number: Int = 5                // Int
 	val HeightDouble: Double = 5.99    // Double
-	val Name: Char = 'D'          // Char
+	val Name: Char = 'D'          // Characters
 	val BooleanValues: Boolean = true     // Boolean
 	val Location: String = "Mumbai"      // String
 }
