9aa4395745b7577a8023d454c37aec95d742dc50
Jatin koli <kolijatin389@gmail.com>
2024-02-14 21:10:06 +0530

    Create Assignment.kt

---
+++ Kotlin/Operators/Assignment.kt
@@ -0,0 +1,10 @@
+	// Assignment operators
+
+
+Operator	Example		Same As	
+=		x = 5		x = 5	
++=		x += 3		x = x + 3	
+-=		x -= 3		x = x - 3	
+*=		x *= 3		x = x * 3	
+/=		x /= 3		x = x / 3	
+%=		x %= 3		x = x % 3	
