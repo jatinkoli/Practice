285047680b33ff380d62306d9e190f08478cd178
Jatin koli <kolijatin389@gmail.com>
2024-02-14 21:10:22 +0530

    Create Logical.kt

---
+++ Kotlin/Operators/Logical.kt
@@ -0,0 +1,22 @@
+	// Logical Operators
+
+// Logical and (&&)
+	fun main() {  
+  		var x = 50
+  		println(x > 30 && x < 100) 
+	}
+		// returns true because 50 is greater than 30 AND 50 is less than 100
+
+//Logical or (||)
+	fun main() {  
+  		var x = 50
+  		println(x > 30 || x < 40) 
+}
+	// returns true because one of the conditions are true (50 is greater than 30 but 50 is not less than 40)
+
+// 	Logical not !
+	fun main() {  
+  		var jatin = true
+  		println(!jatin)
+}
+	 // returns false because the ! (not) operator reverses the result
