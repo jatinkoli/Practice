55211e62b98dfc1f2ab9a5350a64605fdaa9108a
Jatin koli <kolijatin389@gmail.com>
2024-02-14 23:30:56 +0530

    Create String_Length.kt

---
+++ Kotlin/String/String_Length.kt
@@ -0,0 +1,11 @@
+	
+	// String Length
+	
+fun main() {
+
+  var A = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  
+  println("The length of the txt string is: " + A.length)
+  
+}
+	// The length of the A string is: 26 
