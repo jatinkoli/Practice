8f4ee6e771adba02cc027dca7c7e030cbd5d2fa2
Jatin koli <kolijatin389@gmail.com>
2024-02-14 23:35:00 +0530

    Create Comparing_Strings.kt

---
+++ Kotlin/String/Comparing_Strings.kt
@@ -0,0 +1,13 @@
+	
+	//Comparing Strings (compareTo<string>)
+
+fun main()
+{	
+	var FS = "Hello World"
+	var SS = "Hello World"
+		println(FS.compareTo(SS))  
+
+}
+// Outputs 0 (they are equal)
+	
+	
