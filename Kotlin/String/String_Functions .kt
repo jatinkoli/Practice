98d3081ffd565076a63840a42ef6d1f77e5fe9d8
Jatin koli <kolijatin389@gmail.com>
2024-02-14 23:31:02 +0530

    Create String_Functions .kt

---
+++ Kotlin/String/String_Functions .kt
@@ -0,0 +1,12 @@
+	
+	// String Functions
+	
+fun main() {
+
+  var txt = "Jatin koli"
+	println(txt.toUpperCase())
+		// Output : JATIN KOLI
+  
+	println(txt.toLowerCase())
+		// Output : jatin koli
+}
