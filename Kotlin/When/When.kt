db8556e3f72a319a8b911430795e95f483bb87c6
Jatin koli <kolijatin389@gmail.com>
2024-02-19 21:47:34 +0530

    Create When.kt

---
+++ Kotlin/When/When.kt
@@ -0,0 +1,20 @@
+
+
+	// When
+	
+		//Instead of writing many if..else expressions, you can use the when expression, which is much easier to read.
+		
+	fun main(){
+		val day = 4
+		val result = when (day) {
+			1 -> "Monday"
+			2 -> "Tuesday"
+			3 -> "Wednesday"
+			4 -> "Thursday"
+			5 -> "Friday"
+			6 -> "Saturday"
+			7 -> "Sunday"
+		else -> "Invalid day."
+		}
+	println(result)
+	}
