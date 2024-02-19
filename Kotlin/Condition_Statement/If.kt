e84e72426c77cde434dcd4b9190370834c00cab4
Jatin koli <kolijatin389@gmail.com>
2024-02-19 21:23:12 +0530

    Create If.kt

---
+++ Kotlin/Condition_Statement/If.kt
@@ -0,0 +1,13 @@
+
+
+// IF Statement
+
+	// Use if to specify a block of code to be executed if a condition is true
+	
+	
+	var a= 25
+	var b= 23
+	
+	if (a > b) {
+  println(""+a+": is greater than :"+b+)
+}
