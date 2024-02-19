5bff60eb2c90d27f7c7dfa6baf4635c5864ab91a
Jatin koli <kolijatin389@gmail.com>
2024-02-19 22:24:12 +0530

    Update When.kt

--- Kotlin/When/When.kt
+++ Kotlin/When/When.kt
@@ -3,7 +3,8 @@
 	// When
 	
 		//Instead of writing many if..else expressions, you can use the when expression, which is much easier to read.
-		
+		// The when expression is similar to the switch statement in Java.
+
 	fun main(){
 		val day = 4
 		val result = when (day) {
