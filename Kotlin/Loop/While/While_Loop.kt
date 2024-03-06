4b927c763e73b4956810c6a6aee791164a1e9bdb
Jatin koli <kolijatin389@gmail.com>
2024-03-06 23:55:34 +0530

    Create While_Loop.kt

---
+++ Kotlin/Loop/While/While_Loop.kt
@@ -0,0 +1,39 @@
+
+
+	//While Loop
+	
+	
+package com.example.firstkotlinprogram
+import  java.util.Scanner
+
+fun main(args: Array<String>)
+{
+	 var j = 5
+    while (j>0)
+    {
+        println(j)
+        j--
+    }
+		//THE OUTPUT1 IS HAVING THIS CODE OUTPUT
+
+	
+----------------------Factorial code using while----------------------
+	// Dont run it with the up code comment the up code or delete it 
+
+    var IP = Scanner(System.`in`)
+    print("Enter a value you want to display the factorial: ")
+    var FI:Int = IP.nextInt()
+    var i = 1
+    var Fact = 1
+
+    while (i <= FI)
+    {
+        Fact *= i
+        println(""+i+"! = "+Fact)
+        i++
+    }
+		//THE OUTPUT2 IS HAVING THIS CODE OUTPUT
+
+
+
+}
