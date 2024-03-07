0a93db94cae4682ae017abf92e8ce62e5b4afbc2
Jatin koli <kolijatin389@gmail.com>
2024-03-07 22:54:08 +0530

    Create Do-While.kt

---
+++ Kotlin/Loop/Do_While/Do-While.kt
@@ -0,0 +1,21 @@
+
+		//Do_While
+		
+		
+
+package com.example.firstkotlinprogram
+import  java.util.Scanner
+
+fun main(args: Array<String>)
+{   
+   var n = 1
+
+    do{
+        print(""+n+",")
+        n++
+    } while (n <= 20)
+
+
+
+
+}
