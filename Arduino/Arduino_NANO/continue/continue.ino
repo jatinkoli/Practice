e44384b51f01ff98154477b1ad8dcf88b1b25fa2
Jatin koli <kolijatin389@gmail.com>
2024-08-29 21:21:21 +0530

    Create continue.ino

---
+++ Arduino/Arduino_NANO/continue/continue.ino
@@ -0,0 +1,35 @@
+int ledPin = 9;
+
+void setup(){
+
+}
+
+void loop(){
+
+for(int fade=0 ; fade<=255 ; fade+=5)
+
+{
+
+if(fade>40 && fade<120){
+
+continue;
+
+}
+
+analogWrite(ledPin,fade);
+
+delay(30);
+
+}
+
+for(int fade=255 ; fade>=0 ; fade-=5)
+
+{
+
+analogWrite(ledPin,fade);
+
+delay(30);
+
+}
+
+}
